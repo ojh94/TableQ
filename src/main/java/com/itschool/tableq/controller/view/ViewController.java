36de66e290e0f928d10a8db9396e8660c74442b6
Lee Seungjong <lsjjong8@naver.com>
2024-10-18 10:26:09 +0900

    front-end welcome page 추가

--- src/main/java/com/itschool/tableq/controller/view/ViewController.java
+++ src/main/java/com/itschool/tableq/controller/view/ViewController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+    @GetMapping("/")
+    public String main() {
+        return "welcome";
+    }
     @GetMapping("/index")
     public String index() {
         return "index";
