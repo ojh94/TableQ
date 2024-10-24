2d24cac4e977b9b8b5bb81d1baa7575ba51f8b00
Lee Seungjong <lsjjong8@naver.com>
2024-10-24 15:36:24 +0900

    @Operation 어노테이션 추가하여 한글 설명 추가

--- src/main/java/com/itschool/tableq/controller/CrudController.java
+++ src/main/java/com/itschool/tableq/controller/CrudController.java
@@ -4,6 +4,7 @@ import com.itschool.tableq.ifs.CrudInterface;
 import com.itschool.tableq.network.Header;
 import com.itschool.tableq.service.base.BaseService;
 import io.micrometer.observation.annotation.Observed;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,7 @@ public abstract class CrudController<Req, Res, Entity> implements CrudInterface<
     protected BaseService<Req,Res,Entity> baseService;
 
     @Override
+    @Operation(summary = "생성", description = "새로운 엔티티를 생성")
     @PostMapping("")
     public Header<Res> create(@RequestBody Header<Req> request){
         log.info("{}","{}",request);
@@ -23,6 +25,7 @@ public abstract class CrudController<Req, Res, Entity> implements CrudInterface<
     }
 
     @Override
+    @Operation(summary = "읽기", description = "ID로 엔티티를 조회")
     @GetMapping("{id}")
     public Header<Res> read(@PathVariable Long id){
         log.info("{}","{}","read: ",id);
@@ -30,6 +33,7 @@ public abstract class CrudController<Req, Res, Entity> implements CrudInterface<
     }
 
     @Override
+    @Operation(summary = "업데이트", description = "ID로 엔티티를 업데이트")
     @PutMapping("{id}")
     public Header<Res> update(@PathVariable Long id,
                               @RequestBody Header<Req> request) {
@@ -39,6 +43,7 @@ public abstract class CrudController<Req, Res, Entity> implements CrudInterface<
 
     @Override
     @DeleteMapping("{id}")
+    @Operation(summary = "삭제", description = "ID로 엔티티를 삭제")
     public Header delete(@PathVariable Long id) {
         log.info("{}","{}","delete: ",id);
         baseService.delete(id);
--- src/main/java/com/itschool/tableq/controller/api/UserApiController.java
+++ src/main/java/com/itschool/tableq/controller/api/UserApiController.java
@@ -7,17 +7,19 @@ import com.itschool.tableq.network.Response.UserResponse;
 import com.itschool.tableq.network.request.UserRequest;
 import com.itschool.tableq.service.UserService;
 import groovy.util.logging.Slf4j;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Tag(name = "사용자", description = "사용자 관련 API") // 문서에서 쉽게 찾도록 한글로 했음
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController extends CrudController<UserRequest, UserResponse, User> {
+    @Operation(summary = "이메일 중복 확인", description = "email이 기존에 가입되었는지 확인")
     @GetMapping("/check-email")
     public Header<Boolean> isDuplicated(@RequestParam String email){
         return Header.OK(((UserService)baseService).checkEmail(email));
