956b65a6679c5e644d90b66be29dc0481ae608db
Woo Jeongwook <dnwjddnr115@naver.com>
2024-10-31 14:27:46 +0900

    RestaurantImageRepository 수정

--- src/main/java/com/itschool/tableq/repository/RestaurantImageRepository.java
+++ src/main/java/com/itschool/tableq/repository/RestaurantImageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface RestaurantImageRepository extends JpaRepository {
+public interface RestaurantImageRepository extends JpaRepository<RestaurantImage, Long> {
     Optional<List<RestaurantImage>> findByRestaurant(Restaurant restaurant);
 }
