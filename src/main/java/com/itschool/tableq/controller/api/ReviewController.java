44f60a7940322a0fddf44a7291e90e8ec602a391
Lee Seungjong <lsjjong8@naver.com>
2024-11-01 14:41:47 +0900

    CrudController 수정, ReviewController 수정, MenuItem 관련 API 작성

--- src/main/java/com/itschool/tableq/controller/api/ReviewController.java
+++ src/main/java/com/itschool/tableq/controller/api/ReviewController.java
@@ -2,17 +2,29 @@ package com.itschool.tableq.controller.api;
 
 import com.itschool.tableq.controller.CrudController;
 import com.itschool.tableq.domain.Review;
+import com.itschool.tableq.network.Header;
 import com.itschool.tableq.network.request.ReviewRequest;
 import com.itschool.tableq.network.response.ReviewResponse;
+import com.itschool.tableq.service.ReviewService;
 import groovy.util.logging.Slf4j;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @Tag(name = "리뷰 ", description = "리뷰 관련 API")
 @RestController
 @RequestMapping("/api/review")
 public class ReviewController extends CrudController<ReviewRequest, ReviewResponse, Review> {
-
+    @Operation(summary = "레스토랑별 리뷰 조회", description = "Restaurant ID로 엔티티 목록을 조회")
+    @GetMapping("/restaurant/{id}")
+    public Header<List<ReviewResponse>> readByRestaurantId(@PathVariable(name = "id") Long id){
+        log.info("{}","{}","read: ",id);
+        return ((ReviewService)baseService).readByRestaurantId(id);
+    }
 }
--- src/main/java/com/itschool/tableq/domain/MenuItem.java
+++ src/main/java/com/itschool/tableq/domain/MenuItem.java
@@ -1,12 +1,13 @@
 package com.itschool.tableq.domain;
 
+import com.itschool.tableq.network.request.MenuItemRequest;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Table(name = "menu_items")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 @Getter
 @Entity
 public class MenuItem {
@@ -25,4 +26,14 @@ public class MenuItem {
 
     private String imageUrl;
 
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    public void update(MenuItemRequest menuItemRequest) {
+        this.name = menuItemRequest.getName();
+        this.price = menuItemRequest.getPrice();
+        this.description = menuItemRequest.getDescription();
+        this.imageUrl = menuItemRequest.getImageUrl();
+    }
 }
