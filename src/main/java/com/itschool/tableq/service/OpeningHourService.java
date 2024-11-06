a10b63d116645cf5a02a91e0f2f8c0f421c62e01
OHJAEHEON <mykd2012@gmail.com>
2024-11-06 14:22:38 +0900

    OpeningHour 관련수정

--- src/main/java/com/itschool/tableq/service/OpeningHourService.java
+++ src/main/java/com/itschool/tableq/service/OpeningHourService.java
@@ -1,20 +1,53 @@
 package com.itschool.tableq.service;
 
 import com.itschool.tableq.domain.OpeningHour;
+import com.itschool.tableq.domain.Restaurant;
+import com.itschool.tableq.domain.Review;
 import com.itschool.tableq.network.Header;
+import com.itschool.tableq.network.Pagination;
 import com.itschool.tableq.network.request.OpeningHourRequest;
 import com.itschool.tableq.network.response.OpeningHourResponse;
+import com.itschool.tableq.network.response.ReviewResponse;
+import com.itschool.tableq.repository.OpeningHoursRepository;
+import com.itschool.tableq.repository.RestaurantRepository;
+import com.itschool.tableq.repository.ReviewRepository;
 import com.itschool.tableq.service.base.BaseService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class OpeningHourService extends BaseService<OpeningHourRequest, OpeningHourResponse, OpeningHour> {
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    public Header<List<OpeningHourResponse>> readByRestaurantId(Long restaurantId, Pageable pageable) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new EntityNotFoundException("레스토랑을 찾을 수 없습니다."));
+
+        Page<OpeningHour> entities = ((OpeningHoursRepository)baseRepository).findByRestaurant(restaurant, pageable);
+
+        List<OpeningHourResponse> OpeningHourList = entities.stream()
+                .map(entity -> response(entity))
+                .collect(Collectors.toList());
+
+        Pagination pagination = Pagination.builder()
+                .totalPages(entities.getTotalPages())
+                .totalElements(entities.getTotalElements())
+                .currentPage(entities.getNumber())
+                .currentElements(entities.getNumberOfElements())
+                .build();
+
+        return Header.OK(OpeningHourList, pagination);
+    }
 
     @Override
     public Header<List<OpeningHourResponse>> getPaginatedList(Pageable pageable) {
