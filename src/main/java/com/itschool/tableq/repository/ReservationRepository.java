5395fb0395e2261cbde2b2c527be0974dcca0813
Lee Seungjong <lsjjong8@naver.com>
2024-11-12 17:39:57 +0900

    예약 관련 소스 추가 및 수정

--- src/main/java/com/itschool/tableq/repository/ReservationRepository.java
+++ src/main/java/com/itschool/tableq/repository/ReservationRepository.java
@@ -5,12 +5,17 @@ import com.itschool.tableq.domain.Restaurant;
 import com.itschool.tableq.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Optional<List<Reservation>> findByRestaurant(Restaurant restaurant);
     Optional<List<Reservation>> findByUser(User user);
+
+    // userId, RestaurantId, LocalDateTime
+    List<Reservation> findByUserAndRestaurantAndCreatedAtBetween(User user, Restaurant restaurant,
+                                                                 LocalDateTime startOfDay, LocalDateTime endOfDay);
 
     Optional<Reservation> getFirstByOrderByIdDesc();
 
