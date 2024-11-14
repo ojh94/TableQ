b05f7ce3b7fa9c614b497c31c096c902d90fbc29
Woo Jeongwook <dnwjddnr115@naver.com>
2024-11-14 17:15:02 +0900

    Reservation 나의 대기열 조회 API 추가

--- src/main/java/com/itschool/SpringBootDeveloperApplication.java
+++ src/main/java/com/itschool/SpringBootDeveloperApplication.java
@@ -11,5 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
+
     }
 }
