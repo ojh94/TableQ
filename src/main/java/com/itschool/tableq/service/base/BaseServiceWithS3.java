9a07164b0acc84ef3db227830a9789f588d10c9f
Lee Seungjong <lsjjong8@naver.com>
2024-11-22 13:22:06 +0900

    MenuItemApi 관련 파일 업로드, 업데이트, 삭제 구현(test를 위한 s3-file-test.html도 추가)

--- src/main/java/com/itschool/tableq/service/base/BaseServiceWithS3.java
+++ src/main/java/com/itschool/tableq/service/base/BaseServiceWithS3.java
@@ -5,6 +5,7 @@ import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.itschool.tableq.domain.base.IncludeFileUrl;
+import com.itschool.tableq.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,14 +17,40 @@ public abstract class BaseServiceWithS3<Req, Res, Entity extends IncludeFileUrl>
     private AmazonS3Client amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    private String bucketName;
+
+    private String generateS3Key(MultipartFile file, String directoryName, String newFileName) {
+        return directoryName + "/" + newFileName;
+    }
+
+    private String generateS3Url(String fileKey) {
+        return "https://" + bucketName + ".s3.amazonaws.com/" + fileKey;
+    }
+
+    private String extractFileKey(String fileUrl) {
+        String prefix = "https://" + bucketName + ".s3.amazonaws.com/";
+        return fileUrl.replace(prefix, "");
+    }
+
+    private String extractDirectoryNameFromS3Url(String fileUrl) {
+        String fileKey = extractFileKey(fileUrl);
+        int lastSlashIndex = fileKey.lastIndexOf('/');
+        return (lastSlashIndex != -1) ? fileKey.substring(0, lastSlashIndex) : "";
+    }
+
+    private String extractFileNameFromS3Url(String fileUrl) {
+        String fileKey = extractFileKey(fileUrl);
+        int lastSlashIndex = fileKey.lastIndexOf('/');
+        return (lastSlashIndex != -1) ? fileKey.substring(lastSlashIndex + 1) : fileKey;
+    }
 
     public String uploadFile(MultipartFile file, String directoryName, String newFileName) throws IOException, SdkClientException, AmazonServiceException {
+
         // 디렉토리 경로 포함한 S3 객체 키 설정
-        String fileKey = directoryName + "/" + newFileName;
+        String fileKey = generateS3Key(file, directoryName, newFileName);
 
         // S3 URL 생성
-        String fileUrl = "https://" + bucket + ".s3.amazonaws.com/" + fileKey;
+        String s3Url = generateS3Url(fileKey);
 
         // 메타데이터 설정
         ObjectMetadata metadata = new ObjectMetadata();
@@ -31,26 +58,37 @@ public abstract class BaseServiceWithS3<Req, Res, Entity extends IncludeFileUrl>
         metadata.setContentLength(file.getSize());
 
         // S3에 파일 업로드
-        amazonS3Client.putObject(bucket, fileKey, file.getInputStream(), metadata);
+        if(!amazonS3Client.doesObjectExist(bucketName, s3Url)) {
+            amazonS3Client.putObject(bucketName, fileKey, file.getInputStream(), metadata);
+        } else {
+            throw new RuntimeException("S3에 같은 파일 키가 존재합니다.");
+        }
 
-        return fileUrl;
+        return s3Url;
     }
 
     // 파일 수정 (기존 파일을 삭제하고 새 파일을 업로드)
-    public String updateFile(Entity entity, MultipartFile newFile, String directoryName, String newFileName) throws IOException, SdkClientException, AmazonServiceException {
+    public String updateFile(String s3Url, MultipartFile newFile) throws IOException, SdkClientException, AmazonServiceException {
+
+        String directoryName = extractDirectoryNameFromS3Url(s3Url);
+        String newFileName = extractFileNameFromS3Url(s3Url);
+
+        // 대상 파일 존재 여부
+        boolean isExist = amazonS3Client.doesObjectExist(bucketName, directoryName + "/" + newFileName);
+
+        if(isExist) {
+            // 기존 파일을 S3에서 삭제
+            deleteFile(s3Url);
+        }
         // 기존 파일 메타데이터 조회
-        // 기존 파일을 S3에서 삭제
-        amazonS3Client.deleteObject(bucket, entity.getFileUrl());
 
         // 새로운 파일을 업로드
-        String newFileUrl = uploadFile(newFile, directoryName, newFileName);
-
-        return newFileUrl;
+        return uploadFile(newFile, directoryName, newFileName);
     }
 
     // 파일 삭제 (S3와 DB에서 파일 삭제)
-    public void deleteFile(Entity entity) throws SdkClientException, AmazonServiceException {
+    public void deleteFile(String s3Url) throws SdkClientException, AmazonServiceException {
         // S3에서 파일 삭제
-        amazonS3Client.deleteObject(bucket, entity.getFileUrl());
+        amazonS3Client.deleteObject(bucketName, s3Url);
     }
 }
