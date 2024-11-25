2fa17b803b7c712332a60993b95439273546293f
Lee Seungjong <lsjjong8@naver.com>
2024-11-25 16:15:42 +0900

    RestaurantResponse의 DB와 매핑한 필드명 변경 introduction -> information

--- src/main/java/com/itschool/tableq/network/response/RestaurantResponse.java
+++ src/main/java/com/itschool/tableq/network/response/RestaurantResponse.java
@@ -2,36 +2,39 @@ package com.itschool.tableq.network.response;
 
 import com.itschool.tableq.domain.BusinessInformation;
 import com.itschool.tableq.domain.Restaurant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class RestaurantResponse {
     private Long id;
     private String name;
     private String address;
-    private String introduction;
+    private String information;
     private String contactNumber;
     private boolean isAvailable;
     private BusinessInformation businessInformation;
 
-    public RestaurantResponse(Long id, String name, String address, String introduction, String contactNumber,
-                              boolean isAvailable, BusinessInformation businessInformation) {
+    public RestaurantResponse(Long id, String name, String address, String information, String contactNumber,
+                              boolean isAvailable) {
         this.id = id;
         this.name = name;
         this.address = address;
-        this.introduction = introduction;
+        this.information = information;
         this.contactNumber = contactNumber;
         this.isAvailable = isAvailable;
-        this.businessInformation = businessInformation;
     }
 
     public RestaurantResponse(Restaurant restaurant) {
         this.id = restaurant.getId();
         this.name = restaurant.getName();
         this.address = restaurant.getAddress();
-        this.introduction = restaurant.getInformation();
+        this.information = restaurant.getInformation();
         this.contactNumber = restaurant.getContactNumber();
         this.isAvailable = restaurant.isAvailable();
         this.businessInformation = restaurant.getBusinessInformation();
