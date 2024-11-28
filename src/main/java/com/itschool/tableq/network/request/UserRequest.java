c5cb2042e376a6b964340a9ffa036dea53c9df25
Lee Seungjong <lsjjong8@naver.com>
2024-11-28 13:41:56 +0900

    UserRequest 관련 validation 추가

--- src/main/java/com/itschool/tableq/network/request/UserRequest.java
+++ src/main/java/com/itschool/tableq/network/request/UserRequest.java
@@ -19,31 +19,29 @@ public class UserRequest {
     private Long id;
 
     @NotBlank
-    @Size(min=8)
+    @Size(min = 8, max = 16)
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,16}$", message = "비밀번호는 최소 8자 이상, 숫자, 대소문자, 특수문자를 포함해야 합니다.")
     private String password;
+
+    @NotBlank
+    @Email(message = "유효한 이메일 주소를 입력하세요.")
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^[가-힣]+$", message = "이름은 한글만 입력 가능합니다.")
+    private String name; // 이름
 
     private String nickname;
 
     @NotBlank
-    @Pattern(regexp = "^\\+?\\d{10,15}$", message = "유효한 핸드폰 번호를 입력하세요.") // 핸드폰 번호 정규 표현식
+    @Pattern(regexp = "^(01[0-9])-(\\d{3,4})-(\\d{4})$", message = "유효한 한국 핸드폰 번호를 입력하세요.")
     private String phoneNumber;
-
-    private LocalDateTime createdAt;
-
-    private LocalDateTime lastLoginAt;
 
     private String address;
 
-    @NotBlank
-    private String name;
+    /*private String socialType;
 
-    private String socialType;
-
-    private String socialId;
-
-    @NotBlank
-    @Email
-    private String email;
+    private String socialId;*/
 
     private MemberRole memberRole;
 }
